034047428dca4b9a18a88ff2d0c61b9adf5175de
StoryOff <sotic.roma@gmail.com>
2023-03-12 09:15:52 +0800

    refactor test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -6,8 +6,31 @@ urls = [f"{product_base_link}?promo=offer{no}" if no != "???"
         else pytest.param("bugged_link", marks=pytest.mark.xfail) for no in range(10)]
 
 
+@pytest.mark.skip
 @pytest.mark.parametrize("link", urls)
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket_with_promo()
+
+
+@pytest.mark.skip
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/", timeout=0)
+    page.open()
+    page.add_to_basket()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/", timeout=0)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.skip
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/", timeout=0)
+    page.open()
+    page.add_to_basket()
+    page.should_disappeared()
